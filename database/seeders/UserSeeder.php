14160c05f17454d27fdba8a42b275e867d13d635
Amjad Alarori <54799568+amjad-alarori@users.noreply.github.com>
2022-02-10 17:05:01 +0100

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    //    $user = User::create([
+    //        'name' => 'admin',
+    //        'email' => 'admin@clv.com',
+    //        'password' => bcrypt('admin'),
+    //    ]);
+
+    //    $user->attachRole('admin');
+
+    //    $user = User::create([
+    //     'name' => 'marketer',
+    //     'email' => 'marketer@clv.com',
+    //     'password' => bcrypt('marketer'),
+    // ]);
+
+    //     $user->attachRole('marketer');
+
+    //     $user = User::create([
+    //      'name' => 'cleint',
+    //      'email' => 'cleint@clv.com',
+    //      'password' => bcrypt('cleint'),
+    //     ]);
+
+    //     $user->attachRole('cleint');
+
+       
+    }
+}
