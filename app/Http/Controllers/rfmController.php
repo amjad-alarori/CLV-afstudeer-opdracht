a085b2d98af3b42f99ce2fc9e7e652b17f5fc098
Amjad Alarori <54799568+amjad-alarori@users.noreply.github.com>
2022-05-20 21:59:44 +0200

    async controller

--- app/Http/Controllers/rfmController.php
+++ app/Http/Controllers/rfmController.php
@@ -6,13 +6,26 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use App\Models\Rfm;
 use DB;
+use Spatie\Async\Pool;
+
 class rfmController extends Controller
 
 {
 
     public function index(){
 
-        return view ("/dashboard", ["data" => Rfm::all()]);
+        $pool = Pool::create();
+
+        $pool[] = async(function () {
+            return Rfm::all();
+        })->then(function ($output) {
+            $this->results=$output;
+        });
+
+        await($pool);
+
+        return view('/dashboard',['results' => $this->results]);
+//        return view ("/dashboard", ["data" => Rfm::all()]);
     }
 
 
